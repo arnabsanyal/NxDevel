f86965cd42d5a647d79a0cce78982623e969d3e3
Arnab Sanyal <arnabsanyal.ee@iitkgp.ac.in>
2017-09-20 21:02:38 -0700

    Making some progress :)

--- bfsAttempt.c
+++ bfsAttempt.c
@@ -4,28 +4,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct linkedList {
-	int destNode;
-	float weight;
-	struct linkedList *nextPtr;
+struct graphData {
+	int V;
+	int E;
+	int* offsets;
+	float* weight;
+	int* neighbours;
 };
 
-struct linkedList* buildAdjacencyList(struct linkedList* pointerInput, FILE *sourceFile);
-int printList(struct linkedList * pointerInput);
+struct pair {
+	int source;
+	int destination;
+};
+
+struct graphData buildGraph(char *sourceFile);
+int printList(struct graphData inputGraph);
 
 int main(int argc, char *argv[]) {
-	FILE *fp;
-	fp = fopen(argv[1], "r+");
-	int n = atoi(strtok(argv[1],"-"));
-	int *nodeList = (int *)malloc(n * sizeof(int));
-	struct linkedList *adjacencyList = (struct linkedList *)malloc(n * sizeof(struct linkedList));
-	adjacencyList = buildAdjacencyList(adjacencyList, fp);
- 	fclose(fp);
+	struct graphData busData = buildGraph(argv[1]);
+	return 0;
 }
 
-struct linkedList* buildAdjacencyList(struct linkedList* pointerInput, FILE *sourceFile) {
-	char line[LINE_SIZE_MAX];
-	while(fgets(line, LINE_SIZE_MAX, sourceFile))
-		printf("%s\n", line);
-	return pointerInput;
+struct graphData buildGraph(char *sourceFile) {
+	struct graphData populateBus;
+	FILE *fp0 = fopen(sourceFile, "r+");
+	FILE *fp1 = fopen(sourceFile, "r+");
+	populateBus.V = atoi(strtok(sourceFile, "-"));;
+	char ch, *parsed, line[LINE_SIZE_MAX];
+	populateBus.E = 0;
+	while(!feof(fp1)) {
+		ch = fgetc(fp1);
+		if(ch == '\n')
+			populateBus.E++;
+	}
+	fclose(fp1);
+	/*
+	Considering initially equal weight graph ... 
+	Needs to be modified later ...
+	weights field need to be set and the name needs to be checked for w ...
+	*/
+	populateBus.neighbours = (int *)malloc(populateBus.E * sizeof(int));
+	populateBus.offsets = (int *)malloc(populateBus.V * sizeof(int));
+	struct pair *rawData = (struct pair *)malloc(populateBus.E * sizeof(struct pair));
+	/*
+	Let me store the offsets as left indented ...
+	Nodes are numbered from 0 ...
+	More specifically ith node neighbours are at a offset given at ith position in the array ...
+	Store sorted in terms of source vectors in the datatype pair ...
+	*/
+	populateBus.offsets[0] = 0;
+
+	fclose(fp0);
+	return populateBus;
 }
