91d73893f5683b2f7075ef14c6b8cc26f7f6d8ba
Arnab Sanyal <arnabsanyal.ee@iitkgp.ac.in>
2017-09-24 22:20:33 -0700

    Still incomplete but at least the linked list works

--- calleeMain.c
+++ calleeMain.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include "graphKernels.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char const *argv[]) {
 	struct graphData busData = buildGraph(argv[1]);
 	printf("Generated graph status = %d\n\n", printGraph(busData));
-	printf("Queue status = %d\n", shortestPathUnweightedBFS(busData, 0, 0));
+	//printf("Queue status = %d\n", shortestPathUnweightedBFS(busData, 0, 0));
 	return 0;
 }
--- shortestPathThroughBFS.c
+++ shortestPathThroughBFS.c
@@ -3,69 +3,132 @@
 #include <stdlib.h>
 #include "graphKernels.h"
 
-/*================================================================================================================
+/*===============================================================================================================
 Structure Definition(s)
-================================================================================================================*/
+===============================================================================================================*/
 
-struct queue {
-	int nodeNumber;
-	struct queue *link;
+struct node {
+	int vertex;
+	struct node *link;
 };
 
-/*================================================================================================================
+/*===============================================================================================================
 Variable Definition(s)
-================================================================================================================*/
+===============================================================================================================*/
 
-int *nodeList;
+short *nodeList;
+struct node *head;
+struct node *tail;
 
-/*================================================================================================================
+/*===============================================================================================================
 Function Prototype(s)
-================================================================================================================*/
+===============================================================================================================*/
 
-int checkEmptyQueue(struct queue *ref);
-struct queue* initializeQueue();
-int enqueue(struct queue *tail, int newNode);
-struct queue* dequeue(struct queue *head);
+int checkEmptyQueue();
+int initializeQueue();
+int enqueue(int vertex);
+int dequeue();
 
-/*================================================================================================================
-Overriding Function Definition(s)
-================================================================================================================*/
+/*===============================================================================================================
+Function Definition(s)
+===============================================================================================================*/
 
-int checkEmptyQueue(struct queue *ref) {
-	if(ref == NULL)
+int checkEmptyQueue() {
+	if((head == NULL) && (tail == NULL))
 		return 1;
 	else
 		return 0;
 }
 
-struct queue* initializeQueue() {
-	struct queue *ptr = (struct queue*)malloc(sizeof(struct queue));
-	ptr = NULL;
-	return ptr;
-}
-
-int enqueue(struct queue *tail, int newNode) {
-	struct queue *newEntry = (struct queue*)malloc(sizeof(struct queue));
-	newEntry->nodeNumber = newNode;
-	newEntry->link = NULL;
-	(tail->link).link = newEntry;
-	tail->link = newEntry;
+int initializeQueue() {
+	head = NULL;
+	tail = NULL;
 	return 1;
 }
-struct queue* dequeue(struct queue *head) {
 
+int enqueue(int vertex) {
+	struct node *newNode = (struct node*)malloc(sizeof(struct node));
+	newNode->vertex = vertex;
+	newNode->link = NULL;
+	if((head == NULL) && (tail == NULL)) {
+		head = tail = newNode;
+		return 0;
+	}
+	tail->link = newNode;
+	tail = newNode;
+	return 1;
+}
+int dequeue() {
+	struct node *nodeTobeRemoved = head;
+	if((head == NULL) && (tail == NULL))
+		return -1;
+	else
+		head = head->link;
+	int vertex = nodeTobeRemoved->vertex;
+	free(nodeTobeRemoved);
+	if(head == NULL)
+		tail = NULL;
+	return vertex;
 }
 
-int shortestPathUnweightedBFS(struct graphData busData, int destination, int source) {
+/*===============================================================================================================
+Overriding Function Definition(s)
+===============================================================================================================*/
+
+short* shortestPathUnweightedBFS(struct graphData busData, int root) {
+	nodeList = (short *)calloc(busData.V, sizeof(short));
+
+	/*
+	Root distance inside the return array is -1 ...
+	Distance 0 signifies that the point is unreachable ...
+	Must think about this further ...
+	*/
+
+	initializeQueue();
+	enqueue(source);
+	nodeList[source - 1] = 1;
 	int shortestDistance = 0;
+	while(!checkEmptyQueue()) {
+
+	}
 	return shortestDistance;
 }
 
+/*===============================================================================================================
+Temporary Code To Test Linked List
+===============================================================================================================
+
+int main()
+{
+	initializeQueue();
+	printf("Status of Queue %d\n", checkEmptyQueue());
+	enqueue(2);
+	enqueue(5);
+	enqueue(3);
+	printf("FIFO element %d\n", dequeue());
+	printf("Status of Queue %d\n", checkEmptyQueue());
+	enqueue(1);
+	enqueue(7);
+	printf("FIFO element %d\n", dequeue());
+	enqueue(8);
+	enqueue(6);
+	printf("FIFO element %d\n", dequeue());
+	enqueue(9);
+	printf("FIFO element %d\n", dequeue());
+	printf("FIFO element %d\n", dequeue());
+	printf("FIFO element %d\n", dequeue());
+	printf("Status of Queue %d\n", checkEmptyQueue());
+	printf("FIFO element %d\n", dequeue());
+	printf("FIFO element %d\n", dequeue());
+	printf("Status of Queue %d\n", checkEmptyQueue());
+	printf("FIFO element %d\n", dequeue());
+	return 0;
+}
+
+===============================================================================================================*/
+
 /*
 Incomplete ... 
-Enqueue needs attention ...
-Dequeue needs attention ...
-Test Queue structure ...
 Implement BFS ...
 Find unweighted graph distance ...
 */
